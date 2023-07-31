a1adae2b6136fc12892a66e54601bc3f44bdbf32
FiyeroHan <tmddn_00@naver.com>
2023-07-31 20:16:06 +0900

    [Modefied] Conflit

--- NewKiosk/urls.py
+++ NewKiosk/urls.py
@@ -1,5 +1,4 @@
 '''
-<<<<<<< HEAD
 from django.contrib import admin
 from django.urls import path, include
 
@@ -20,13 +19,17 @@ urlpatterns = [
 '''
 from django.urls import path,include
 from django.contrib import admin
-from NewKiosk import views
+
+from .views import CategoryViewSet,  ProductViewSet, OrderViewSet ,Product_OrderViewSet
+
+from rest_framework import routers
+
+routers = routers.DefaultRouter()
+routers.register('category', CategoryViewSet)
+routers.register('product', ProductViewSet)
+routers.register('order', OrderViewSet)
+routers.register('product-order', Product_OrderViewSet)
 
 urlpatterns = [
-    path('api/category/', views.CategoryListAPI.as_view()),
-    path('api/product/', views.ProductListAPI.as_view()),
-    path('api/order/', views.OrderListAPI.as_view()),
-    path('api/product_order/', views.Product_OrderListAPI.as_view()),
+    path('api/', include(routers.urls)),
 ]
-
-# >>>>>>> d0e8621caf4320d5893cc650758fa79c26a5d49a
